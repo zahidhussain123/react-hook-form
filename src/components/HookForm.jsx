f3eceab2710953f651f810807bc817d1b86589df
Zahid Hussain <zahid4307@gmail.com>
2022-08-26 14:38:27 +0500

    formstate used for handling error messages

--- src/components/HookForm.jsx
+++ src/components/HookForm.jsx
@@ -11,7 +11,11 @@ const HookForm = () => {
     password: yup.string().min(5).max(14).required(),
     cpassword: yup.string().oneOf([yup.ref("password"), null]),
   });
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
   });
 
@@ -30,18 +34,27 @@ const HookForm = () => {
     >
       <label>FirstName</label>
       <input type="text" placeholder="First Name" {...register("firstName")} />
+      <small>{errors.firstName?.message}</small>
       <label>LastName</label>
       <input type="text" placeholder="Last Name" {...register("lastName")} />
+      <small>{errors.lastName?.message}</small>
+
       <label>Email</label>
       <input type="text" placeholder="Email" {...register("email")} />
+      <small>{errors.email?.message}</small>
+
       <label>Password</label>
       <input type="text" placeholder="Password" {...register("password")} />
+      <small>{errors.password?.message}</small>
+
       <label>Confirm Password</label>
       <input
         type="text"
         placeholder="Confirm Password"
         {...register("cpassword")}
       />
+      <small>{errors.cpassword?.message}</small>
+
       <button type="submit">Submit</button>
     </form>
   );
