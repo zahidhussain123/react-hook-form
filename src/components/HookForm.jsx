27cddca2241ef74d65e2004dc55ecef3e896effa
Zahid Hussain <zahid4307@gmail.com>
2022-08-26 14:50:35 +0500

    modify the error messages

--- src/components/HookForm.jsx
+++ src/components/HookForm.jsx
@@ -4,12 +4,21 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const HookForm = () => {
+  const styling = {
+    color: "red",
+  };
   const schema = yup.object().shape({
-    firstName: yup.string().max(12).required(),
-    lastName: yup.string().max(12).required(),
-    email: yup.string().email().required(),
-    password: yup.string().min(5).max(14).required(),
-    cpassword: yup.string().oneOf([yup.ref("password"), null]),
+    firstName: yup.string().max(12).required("First Name is required"),
+    lastName: yup.string().max(12).required("last Name is required"),
+    email: yup.string().email().required("Email is required"),
+    password: yup
+      .string()
+      .min(5)
+      .max(14)
+      .required("Password should be at least 5 characters"),
+    cpassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Password is not matching"),
   });
   const {
     register,
@@ -34,18 +43,18 @@ const HookForm = () => {
     >
       <label>FirstName</label>
       <input type="text" placeholder="First Name" {...register("firstName")} />
-      <small>{errors.firstName?.message}</small>
+      <small style={styling}>{errors.firstName?.message}</small>
       <label>LastName</label>
       <input type="text" placeholder="Last Name" {...register("lastName")} />
-      <small>{errors.lastName?.message}</small>
+      <small style={styling}>{errors.lastName?.message}</small>
 
       <label>Email</label>
       <input type="text" placeholder="Email" {...register("email")} />
-      <small>{errors.email?.message}</small>
+      <small style={styling}>{errors.email?.message}</small>
 
       <label>Password</label>
       <input type="text" placeholder="Password" {...register("password")} />
-      <small>{errors.password?.message}</small>
+      <small style={styling}>{errors.password?.message}</small>
 
       <label>Confirm Password</label>
       <input
@@ -53,7 +62,7 @@ const HookForm = () => {
         placeholder="Confirm Password"
         {...register("cpassword")}
       />
-      <small>{errors.cpassword?.message}</small>
+      <small style={styling}>{errors.cpassword?.message}</small>
 
       <button type="submit">Submit</button>
     </form>
